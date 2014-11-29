a626100878d754071b19a64936b5c94ae8808d6b
Damien Ryan <djryan@gmail.com>
2014-11-29 00:37:03 +0000

    Sigil 0.8.2

--- Sigil/tools/chocolateyInstall.ps1
+++ Sigil/tools/chocolateyInstall.ps1
@@ -1,1 +1,7 @@
-Install-ChocolateyPackage 'Sigil' 'exe' '/VERYSILENT' 'https://github.com/user-none/Sigil/releases/download/0.8.1/Sigil-0.8.1-Windows-Setup.exe' 'https://github.com/user-none/Sigil/releases/download/0.8.1/Sigil-0.8.1-Windows-x64-Setup.exe'
+$packageName = "Sigil"
+$installerType = "exe"
+$32BitUrl = "https://github.com/user-none/Sigil/releases/download/0.8.2/Sigil-0.8.2-Windows-Setup.exe"
+$64BitUrl = "https://github.com/user-none/Sigil/releases/download/0.8.1/Sigil-0.8.1-Windows-x64-Setup.exe"
+$silentArgs = "/VERYSILENT"
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl $64BitUrl
