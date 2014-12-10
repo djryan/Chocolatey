fdc27acf29cd81258e330f54347c0066b19b8bc2
Damien Ryan <Damien.Ryan@bromium.com>
2014-12-10 11:53:26 +0000

    Fix Sigil wrongness

--- Sigil/tools/chocolateyInstall.ps1
+++ Sigil/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "Sigil"
 $installerType = "exe"
 $32BitUrl = "https://github.com/user-none/Sigil/releases/download/0.8.2/Sigil-0.8.2-Windows-Setup.exe"
-$64BitUrl = "https://github.com/user-none/Sigil/releases/download/0.8.1/Sigil-0.8.1-Windows-x64-Setup.exe"
+$64BitUrl = "https://github.com/user-none/Sigil/releases/download/0.8.2/Sigil-0.8.2-Windows-x64-Setup.exe"
 $silentArgs = "/VERYSILENT"
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl $64BitUrl
