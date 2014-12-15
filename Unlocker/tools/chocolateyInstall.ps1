03b1dc1d40435cefdd8389ad94105f95593dea17
Damien Ryan <Damien.Ryan@bromium.com>
2014-12-15 10:41:29 +0000

    Contains optional spyware

--- Unlocker/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-$packageName = "Unlocker"
-$installerType = "msi"
-$32BitUrl = "http://www.emptyloop.com/unlocker/download.php"
-$silentArgs = "/qb"
-
-Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl 
